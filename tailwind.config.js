d7bb8e7293c9f25e4800f782552113b8a61cb71c
shahzaib-k <shahzaibkhan.4659@gmail.com>
2024-03-28 23:55:46 +0500

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#B4D4FF',
+        paleblue: '#B4D4FF',
+        palegreen: '#C3E2C2'
+      },
+    },
+  },
+  plugins: [],
+}
